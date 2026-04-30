fc4d99c9327b6e1ee78c24aeb0c460d7d42f0880
Pedro Ennes <pedro@Pedros-MacBook-Pro-3.local>
2026-04-29 21:39:31 -0500

    add team logos

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  images: {
+    domains: ['cdn.nba.com'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
